b1cad1ac3f28c04c72e85bd12b9b024fb056fe97
Aleksei Antonovich <aleksei.antonovich@molecularhealth.com>
2018-07-18 16:43:40 +0200

    added example of class mocking using sinon

---
+++ src/AppInfo.sinon.test.tsx
@@ -0,0 +1,36 @@
+import './inversify.config.ts';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as sinon from "sinon";
+
+import AppInfo from './AppInfo';
+import { UserService } from './userService';
+
+describe('AppInfo tests', () => {
+  let sandbox = sinon.createSandbox();
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<AppInfo />, div);
+    ReactDOM.unmountComponentAtNode(div);
+    const stub = sandbox.stub(UserService.prototype, "foo").callsFake(() => {
+      console.log('I am foo in sinon fake'); // tslint:disable-line
+    });
+    expect(stub.notCalled).toBeTruthy();
+  });
+
+  test('calls public method that calls service', () => {
+    const sut = new AppInfo({});
+    const stub = sandbox.stub(UserService.prototype, "foo").callsFake(() => {
+      console.log('I am foo in sinon fake'); // tslint:disable-line
+    });
+    sut.handleClick();
+    expect(stub.calledOnce).toBeTruthy();
+  })
+});
+
--- src/userService.ts
+++ src/userService.ts
@@ -2,6 +2,10 @@ import { injectable } from 'inversify';
 
 @injectable()
 class UserService {
+  constructor() {
+    console.log('I am UserService constructor'); // tslint:disable-line
+  }
+
   public foo(): string {
     return 'foo'; // tslint:disable-line
   }
